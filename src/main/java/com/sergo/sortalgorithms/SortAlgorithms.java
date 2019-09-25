4140a254d5c9e72134ec81d5818de7866708a974
Sergey Polyakov <poliakov.s.e@yandex.ru>
2019-09-25 18:17:35 +0300

    Added heap sort, added some tests

--- src/main/java/com/sergo/sortalgorithms/SortAlgorithms.java
+++ src/main/java/com/sergo/sortalgorithms/SortAlgorithms.java
@@ -11,6 +11,8 @@ public class SortAlgorithms {
      * @return sorted array
      */
     public static int[] quickSort(int[] array) {
+        if (array.length == 0) return array;
+        if (array == null) throw new NullPointerException();
         quickSort(array, 0, array.length - 1);
         return array;
     }
@@ -63,6 +65,8 @@ public class SortAlgorithms {
      * @return sorted array
      */
     public static int[] mergeSort(int[] array) {
+        if (array.length == 0) return array;
+        if (array == null) throw new NullPointerException();
         mergeSort(array, 0, array.length - 1);
         return array;
     }
@@ -97,15 +101,11 @@ public class SortAlgorithms {
         int lengthLeft = middle - left + 1;
         int lengthRight = right - middle;
 
-        int leftArray[] = new int[lengthLeft];
-        int rightArray[] = new int[lengthRight];
+        int[] leftArray = new int[lengthLeft];
+        int[] rightArray = new int[lengthRight];
 
-        for (int i = 0; i < lengthLeft; i++) {
-            leftArray[i] = array[left + i];
-        }
-        for (int i = 0; i < lengthRight; i++) {
-            rightArray[i] = array[middle + i + 1];
-        }
+        System.arraycopy(array, left, leftArray, 0, lengthLeft);
+        System.arraycopy(array, middle + 1, rightArray, 0, lengthRight);
 
         int leftIndex = 0;
         int rightIndex = 0;
@@ -126,6 +126,62 @@ public class SortAlgorithms {
                 array[i] = rightArray[rightIndex];
                 rightIndex++;
             }
+        }
+        return array;
+    }
+
+    /**
+     * Initial method for heap sort
+     * Best - O(nlogn)
+     * Middle - O(nlogn)
+     * Worse - O(nlogn)
+     *
+     * @param array Unsorted array of int values
+     * @return sorted array
+     */
+    public static int[] heapSort(int[] array) {
+        if (array.length == 0) return array;
+        if (array == null) throw new NullPointerException();
+        // Building a heap
+        int length = array.length;
+        // going from first without new branches to root
+        // now root is a max value of array
+        for (int i = length / 2 - 1; i >= 0; i--)
+            heapify(array, length, i);
+        // Swap root with last array element (max going to the end of array, this element sorted)
+        // Repeat binary heap strucrtue (making root max value in length = i)
+        for (int i = length - 1; i >= 0; i--) {
+            int tmp = array[0];
+            array[0] = array[i];
+            array[i] = tmp;
+
+            heapify(array, i, 0);
+        }
+        return array;
+    }
+
+    /**
+     * Makes a swap if child bigger then parent
+     *
+     * @param array    input array
+     * @param length   length of array
+     * @param parentId id of parent in heap
+     */
+    private static int[] heapify(int[] array, int length, int parentId) {
+        int leftChild = 2 * parentId + 1;
+        int rightChild = 2 * parentId + 2;
+        int largest = parentId;
+        if (leftChild < length && array[leftChild] > array[largest]) {
+            largest = leftChild;
+        }
+        if (rightChild < length && array[rightChild] > array[largest]) {
+            largest = rightChild;
+        }
+        if (largest != parentId) {
+            int tmp = array[parentId];
+            array[parentId] = array[largest];
+            array[largest] = tmp;
+            heapify(array, length, largest);
         }
         return array;
     }
--- src/test/java/com/sergo/sortalgorithms/testSorts.java
+++ src/test/java/com/sergo/sortalgorithms/testSorts.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.*;
 public class testSorts {
     @Test
     public void testQuickSort() {
+        assertArrayEquals(new int[]{},SortAlgorithms.quickSort(new int[]{}));
 
         assertArrayEquals(new int[]{1, 3, 7, 8, 10, 15, 21}, SortAlgorithms.quickSort(new int[]{7, 10, 8, 1, 3, 21, 15}));
 
@@ -19,6 +20,8 @@ public class testSorts {
 
     @Test
     public void testMergeSort(){
+        assertArrayEquals(new int[]{},SortAlgorithms.mergeSort(new int[]{}));
+
         assertArrayEquals(new int[]{1, 3, 7, 8, 10, 15, 21}, SortAlgorithms.mergeSort(new int[]{7, 10, 8, 1, 3, 21, 15}));
 
         assertArrayEquals(new int[]{2, 7, 23, 115, 359, 421, 502, 756, 1280, 5000},
@@ -26,5 +29,18 @@ public class testSorts {
 
         assertArrayEquals(new int[]{-6, -5, -4, -2, -2, 0, 0, 0, 1, 1, 2, 3, 3, 4, 5},
                 SortAlgorithms.mergeSort(new int[]{0, -2, 3, 0, 1, 2, -4, 0, 5, -6, -5, 1, 3, -2, 4}));
+    }
+
+    @Test
+    public void testHeapSort(){
+        assertArrayEquals(new int[]{},SortAlgorithms.heapSort(new int[]{}));
+
+        assertArrayEquals(new int[]{1, 1, 2, 3, 4, 7, 8, 8, 10, 15, 15, 17, 21, 33}, SortAlgorithms.heapSort(new int[]{7, 10, 8, 1, 3, 21, 15, 2, 4, 1, 8, 33, 15,17}));
+
+        assertArrayEquals(new int[]{2, 7, 23, 115, 359, 421, 502, 756, 1280, 5000},
+                SortAlgorithms.heapSort(new int[]{359, 756, 421, 502, 115, 23, 1280, 2, 5000, 7}));
+
+        assertArrayEquals(new int[]{-6, -5, -4, -2, -2, 0, 0, 0, 1, 1, 2, 3, 3, 4, 5},
+                SortAlgorithms.heapSort(new int[]{0, -2, 3, 0, 1, 2, -4, 0, 5, -6, -5, 1, 3, -2, 4}));
     }
 }
